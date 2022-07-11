8dae9740f1933240e2f4d26f7c3f37826de8b327
Takuya Matsuda <matsuda.takuya41@gmail.com>
2022-07-11 15:53:56 +0900

    cp76

--- src/components/atoms/button/PrimaryButton.tsx
+++ src/components/atoms/button/PrimaryButton.tsx
@@ -3,13 +3,19 @@ import { Button } from "@chakra-ui/react";
 
 type Props = {
   children: ReactNode;
+  onClick: () => void;
 };
 
 export const PrimaryButton: FC<Props> = memo((props) => {
-  const { children } = props;
+  const { children, onClick } = props;
 
   return (
-    <Button bg="teal.400" color="white" _hover={{ opacity: 0.6 }}>
+    <Button
+      bg="teal.400"
+      color="white"
+      _hover={{ opacity: 0.6 }}
+      onClick={onClick}
+    >
       {children}
     </Button>
   );
