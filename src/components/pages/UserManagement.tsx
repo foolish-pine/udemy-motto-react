9728829528734828c055ac7c0db9be6721a167d1
Matsuda Takuya <matsuda.takuya41@gmail.com>
2022-07-24 15:43:07 +0900

    cp84

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -10,13 +10,14 @@ import { UserCard } from "../organisms/user/UserCard";
 import { UserDetailModal } from "../organisms/user/UserDetailModal";
 import { useAllUsers } from "../../hooks/useAllUsers";
 import { useSelectUser } from "../../hooks/useSelectUser";
-// import { useLoginUser } from "../../hooks/useLoginUser";
+import { useLoginUser } from "../../hooks/useLoginUser";
 
 export const UserManagement: FC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, loading, users } = useAllUsers();
   const { onSelectUser, selectedUser } = useSelectUser();
-  // const { loginUser } = useLoginUser();
+  const { loginUser } = useLoginUser();
+  console.log(loginUser);
 
   useEffect(() => {
     getUsers();
@@ -50,7 +51,12 @@ export const UserManagement: FC = memo(() => {
           ))}
         </Wrap>
       )}
-      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
+      <UserDetailModal
+        user={selectedUser}
+        isAdmin={loginUser?.isAdmin}
+        isOpen={isOpen}
+        onClose={onClose}
+      />
     </>
   );
 });
